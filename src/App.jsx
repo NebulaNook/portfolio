a857aa0bb93e132b4f791c9bc5f29a9ad3906d4c
Nebulanook <kaushal98211@gmail.com>
2025-07-01 18:18:45 +0545

    typo error

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
-import MainLayout from "./Layouts/MainLayout";
+import MainLayout from "./Layouts/Mainlayout";
 import Home from "./pages/Home";
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainLayout />}>
+        <Route path="/" element={<Mainlayout />}>
           <Route index element={<Home />} />
         </Route>
       </Routes>
