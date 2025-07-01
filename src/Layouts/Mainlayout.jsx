f067f5ecc9f32c6e90844acaa43a5fdaa918361f
Nebulanook <kaushal98211@gmail.com>
2025-07-01 18:25:20 +0545

    fix typo error

--- src/Layouts/Mainlayout.jsx
+++ src/Layouts/Mainlayout.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/Navbar";
-import Contact from "../Components/Contact";
+import Contact from "../components/Contact";
 import { Outlet } from "react-router-dom";
 export default function MainLayout() {
   return (
